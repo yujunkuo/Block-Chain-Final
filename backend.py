56225531eebca435c9131b8d9f24ae57c07ae628
yujunkuo <johnboy880313@gmail.com>
2021-06-11 20:48:27 +0800

    Try to compile solidity file

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 import json
 from web3 import Web3
-from solc import compile_files, link_code
+from solc import install_solc, compile_files, link_code
+
+install_solc('v0.4.25')
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 
